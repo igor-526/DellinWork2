0ba93e301af08b0ba7737db1a7d8f387edadafa0
Igor Ter-Pogosyan <igor-526@yandex.ru>
2025-03-02 21:26:00 +0300

    Preparing for deploy

--- telegram_bot/handlers/commands/menu.py
+++ telegram_bot/handlers/commands/menu.py
@@ -2,7 +2,6 @@ from aiogram import types, Router, F
 from aiogram.filters import StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.filters import Command
-
 from telegram_bot.funcs.calculate.calculate_messages import calculate_start_message
 from telegram_bot.funcs.menu import send_menu
 
@@ -19,6 +18,36 @@ async def h_menu_reset(message: types.Message, state: FSMContext):
                 F.text == "Расчёт путевого листа")
 async def h_menu_calculate(message: types.Message, state: FSMContext) -> None:
     await calculate_start_message(message, state)
+
+
+@router.message(StateFilter(None),
+                F.text == "Добавить рабочее время")
+async def h_menu_worktime(message: types.Message, state: FSMContext) -> None:
+    await message.answer("Функция находится в разработке")
+
+
+@router.message(StateFilter(None),
+                F.text == "Контакты")
+async def h_menu_contacts(message: types.Message, state: FSMContext) -> None:
+    await message.answer("Функция находится в разработке")
+
+
+@router.message(StateFilter(None),
+                F.text == "Ближайшая шинка/мойка")
+async def h_menu_wash_repair(message: types.Message, state: FSMContext) -> None:
+    await message.answer("Функция находится в разработке")
+
+
+@router.message(StateFilter(None),
+                F.text == "Настройки")
+async def h_menu_settings(message: types.Message, state: FSMContext) -> None:
+    await message.answer("Функция находится в разработке")
+
+
+@router.message(StateFilter(None),
+                F.text == "Отчёты")
+async def h_menu_reports(message: types.Message, state: FSMContext) -> None:
+    await message.answer("Функция находится в разработке")
 
 
 @router.message(StateFilter(None))
